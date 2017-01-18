f60805cc1914fff9c5f35f777558abeb42e145ed
Eukavlin <brodin.elise@gmail.com>
2017-01-18 22:21:17 +0100

    dataprovence httpS link

--- app.js
+++ app.js
@@ -117,7 +117,7 @@ $(document).ready(function() {
 function LoadAdditionalData()
 {
     //create the queryUrl to be used in the service call
-    var query = "http://dataprovence.cloudapp.net:8080/v1/dataprovencetourisme/ParcsEtJardins?&format=json&callback=?";
+    var query = "https://dataprovence.cloudapp.net:8080/v1/dataprovencetourisme/ParcsEtJardins?&format=json&callback=?";
     var filter = "";
     var queryUrl = query + filter;
     //make jquery call to service
