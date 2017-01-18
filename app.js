ad6e269abdd67b14a0c349056d95446f5c83e1c4
Eukavlin <brodin.elise@gmail.com>
2017-01-18 22:16:00 +0100

    issue list appearance solved

--- app.js
+++ app.js
@@ -108,9 +108,7 @@ function mySubmitFunction(evt) {
 }
 
 $(document).ready(function() {
-  $(document).on("submit", '#citySearch', function(event){
     LoadAdditionalData();
-    event.preventDefault();
 });
 
 
